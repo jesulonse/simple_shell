9c15c1ddbe82049e67fc1b92600a12418262a116
jesulonse <jesulonse@gmail.com>
2022-11-16 16:15:02 -0800

    updates in simple shell

--- getLine.c
+++ getLine.c
@@ -18,6 +18,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 		/*bfree((void **)info->cmd_buf);*/
 		free(*buf);
 		*buf = NULL;
+		signal(SIGINT, sigintHandler);
 		/**strsignal(SIGINT, sigintHandler);*/
 #if USE_GETLINE
 		r = getline(buf, &len_p, stdin);
